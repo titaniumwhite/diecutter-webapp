4f4d8a7726983d7c6143d68eee23e0d912482995
Gabriele De Santis <gabriele1.desantis@gmail.com>
2021-04-06 11:39:37 +0200

    Implemented session number

--- influx.js
+++ influx.js
@@ -12,7 +12,8 @@ const influx = new Influx.InfluxDB({
           acceleration_y: Influx.FieldType.FLOAT,
           acceleration_z: Influx.FieldType.FLOAT,
           sequence_number: Influx.FieldType.INTEGER,
-          rounds: Influx.FieldType.INTEGER
+          rounds: Influx.FieldType.INTEGER,
+          session: Influx.FieldType.INTEGER
         },
         tags: [
           'host'
@@ -26,12 +27,13 @@ function write(data) {
         {
             measurement: 'ruuvi',
             fields: { 
-                mac: data['mac'],
-                acceleration_x: data['acceleration_x'],
-                acceleration_y: data['acceleration_y'],
-                acceleration_z: data['acceleration_z'],
-                sequence_number: data['sequence_number'],
-                rounds: data['rounds']
+              mac: data['mac'],
+              acceleration_x: data['acceleration_x'],
+              acceleration_y: data['acceleration_y'],
+              acceleration_z: data['acceleration_z'],
+              sequence_number: data['sequence_number'],
+              rounds: data['rounds'],
+              session: data['session']
             }
         }
     ], {
@@ -39,6 +41,13 @@ function write(data) {
     });
 }
 
+function readSession() {
+  let query = influx.query('select * from ruuvi order by desc limit 1');
+  if (query.hasOwnProperty('session')) return (query[0])['session'];
+  else return 1;
+}
+
 module.exports = {
-  write
+  write,
+  readSession
 }
--- noblejs.js
+++ noblejs.js
@@ -1,6 +1,7 @@
 const noble = require('@abandonware/noble');
 const influx = require('./influx');
 const { EventEmitter } = require('events');
+const { get } = require('jquery');
 
 const statusEmitter = new EventEmitter();
 
@@ -8,9 +9,9 @@ let actual_mac = {};
 let temporary_mac = [];
 let idInterval; // after 15 seconds, delete the unnecessary objects in the dectionary and initialize the list
 let idTimeout; // if there are no packets for 45 seconds, the connection is considered as lost
-let first_scan = true;
 let first_data = true;
 let last_round_value = -1;
+let session = influx.readSession();
 
 function explore() {
   noble.on('stateChange', async function (state) {
@@ -31,43 +32,47 @@ function on_discovery(peripheral) {
   // ignore devices with no manufacturer data
   if (!peripheral.advertisement.manufacturerData || peripheral.advertisement.manufacturerData[0] != 0x99) 
     return;
-
+  
   let encoded_data = peripheral.advertisement.manufacturerData;
 
   if (encoded_data[0] == 0x99 && encoded_data[1] == 0x04 && encoded_data[2] == 5) {
     
-    // if no packets for 60 seconds, device is disconnected
+    // if no packets for 45 seconds, device is disconnected
     clearTimeout(idTimeout);
-    idTimeout = setTimeout(reset, 30000);
+    idTimeout = setTimeout(reset, 45000);
 
     // first ruuvi packet received
     if (first_data) {
       idInterval = setInterval(checkList, 15000);
       first_data = false;
     }
+        
+    updateList(temporary_mac, peripheral.address);
 
     statusEmitter.emit('connected', peripheral.address);
     
     let data_slice = encoded_data.slice(2);
 
-    decoded_data = decode_data(data_slice);
+    decoded_data = decode(data_slice);
     decoded_data["mac"] = peripheral.address;
 
     //console.log('Packet from ' + peripheral.address + ' RSSI -> ' + peripheral.rssi);
 
-    updateList(temporary_mac, peripheral.address);
-
     // write in the database only the packet of the closer device
     if ((peripheral.address === updateDictionary(actual_mac, peripheral.address, peripheral.rssi))
           && last_round_value !== decoded_data["rounds"]) {
+      
       //console.log("Writing on Influx the data of " + peripheral.address);
+
+      if (decoded_data["rounds"] == 0) session++;
+      decoded_data["session"] = session;
       influx.write(decoded_data);
     }
     last_round_value = decoded_data["rounds"];
   }
 }
 
-function decode_data(data) {
+function decode(data) {
   let measurement = {};
 
   measurement["data_format"] = data.slice(0, 1).readInt8();
@@ -97,7 +102,7 @@ function decode_data(data) {
 
 async function reset() {
   statusEmitter.emit('disconnected');
-  console.err("Disconnected from RuuviTag.");
+  console.error("Disconnected from RuuviTag.");
   clearInterval(idInterval);
   clearInterval(idTimeout);
   first_data = true;
